3528a836a4913cc105249c562d583f265357992a
belizc <54868202+belizc@users.noreply.github.com>
2020-09-11 13:42:30 -0400

    Update hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Family</title>
+	<meta charset="UTF-8">
+	
+<body>
+<p>In 2016 I loaded my kids into the minivan and took them on a drive across the Northern midwest states.
+</body>
+</html>
